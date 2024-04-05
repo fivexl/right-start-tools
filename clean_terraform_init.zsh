7af1af80f0ff9fc284f7cd8d58b7f3919f715304
Anton Eremin <mobessona2@gmail.com>
2024-04-05 10:41:31 +0500

    feat: zsh to clean .terraform terraform.lock.hcl in every directory

---
+++ clean_terraform_init.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+#feat: zsh to clean .terraform terraform.lock.hcl in every directory of the project
+
+# Base directories to search in
+base_dirs=("modules" "organization")
+
+# Loop through base directories
+for base_dir in "${base_dirs[@]}"; do
+  # Find and delete .terraform directories recursively
+  find "$base_dir" -type d -name ".terraform" -print -exec rm -rf {} \;
+
+  # Find and delete .terraform.lock.hcl files recursively
+  find "$base_dir" -type f -name ".terraform.lock.hcl" -print -exec rm -f {} \;
+done
